1a611126eb0b358bf099c5b31073e7b9d10e0862
Ruslan Yupyn <r.yupyn@crossroadlabs.xyz>
2017-02-22 16:33:20 +0200

    added OSX shared scheme;

---
+++ Spectre-osx/Spectre-osx.h
@@ -0,0 +1,19 @@
+//
+//  Spectre-osx.h
+//  Spectre-osx
+//
+//  Created by Ruslan Yupyn on 2/22/17.
+//  Copyright © 2017 CrossroadLabs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Spectre-osx.
+FOUNDATION_EXPORT double Spectre_osxVersionNumber;
+
+//! Project version string for Spectre-osx.
+FOUNDATION_EXPORT const unsigned char Spectre_osxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spectre_osx/PublicHeader.h>
+
+
